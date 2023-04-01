f623580fccf3ec01e8acb79e5d7bc4a0292281f2
Tony Rodriguez <trodrig4@uccs.edu>
2023-04-01 23:57:41 +0000

    Added image as attribute and implemented bootstrap into all views

---
+++ app/models/book.rb
@@ -0,0 +1,9 @@
+class Book < ApplicationRecord
+    validates :description, presence: true, length: { minimum: 20 }
+    validates :title, presence: true
+    validates :rating, numericality: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to:10 }
+    validates :image, presence: true
+    #validates :date, presence: true
+    has_one_attached :image
+    
+end
